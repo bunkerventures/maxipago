f906ee3d034cdf8675924798cabb3d50a26d95c3
Jhonathas Matos <jhonathas@gmail.com>
2015-04-13 16:16:54 -0300

    Added new command

--- lib/maxipago/xml_builder/builder.rb
+++ lib/maxipago/xml_builder/builder.rb
@@ -6,7 +6,7 @@ module Maxipago
 
       attr_reader :maxid, :apikey, :apiversion, :command, :options
 
-      MAXIPAGO_COMMANDS = %w(add_consumer delete_consumer update_consumer add_card_onfile delete_card_onfile sale void reversal recurring cancel_recurring bank_bill online_debit save_on_file one_transaction_report list_transactions_report transaction_paginate authorization authentication capture)
+      MAXIPAGO_COMMANDS = %w(add_consumer delete_consumer update_consumer add_card_onfile delete_card_onfile sale void reversal recurring cancel_recurring bank_bill online_debit save_on_file one_transaction_report one_order_report list_transactions_report transaction_paginate authorization authentication capture)
 
       def initialize(maxid, apikey, apiversion, opts)
         @maxid = maxid
